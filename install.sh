5a42fe89b21f6cea3278a5a7df3307a5a41f6ebf
dm4 <sunrisedm4@gmail.com>
2014-08-28 14:16:46 +0800

    if [ ... ]; then

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 for file in "${DIR}"/*
 do
-    if [[ "$(basename $file)" != "install.sh" ]]
-    then
+    if [[ "$(basename $file)" != "install.sh" ]]; then
         filename="$(basename "$file")"
         target_filename="${HOME}/.${filename}"
 
-        if [[ -f "$target_filename" ]]
-        then
+        if [[ -f "$target_filename" ]]; then
             echo "${target_filename} already exist."
         else
             echo "ln -s \"$file\" \"$target_filename\""
