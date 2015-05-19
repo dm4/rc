cf5b2f0a905a5dff5f114fc8e6fff0873fa53feb
dm4 <sunrisedm4@gmail.com>
2015-05-19 12:46:00 +0800

    add force install option

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+
+# get options
+while getopts ":f" opt; do
+    case $opt in
+        # force
+        f)
+            echo "Force install..."
+            force=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# get dirname
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# install all files
 for file in "${DIR}"/*
 do
     if [[ "$(basename $file)" != "install.sh" ]]; then
@@ -7,7 +26,14 @@ do
         target_filename="${HOME}/.${filename}"
 
         if [[ -f "$target_filename" ]]; then
-            echo "${target_filename} already exist."
+            if [[ -n "$force" ]]; then
+                echo "rm -f \"$target_filename\""
+                rm -f "$target_filename"
+                echo "ln -s \"$file\" \"$target_filename\""
+                ln -s "$file" "$target_filename"
+            else
+                echo "${target_filename} already exist."
+            fi
         else
             echo "ln -s \"$file\" \"$target_filename\""
             ln -s "$file" "$target_filename"
