35b12a94eb7ba3d32830e790f914a300c36bf3cd
dm4 <sunrisedm4@gmail.com>
2024-09-24 22:18:58 +0800

    wezterm: adjust tab bar width

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,10 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Update
 config.check_for_updates = true
+
+-- Fonts, colors, and styles
 config.font = wezterm.font('SauceCodePro Nerd Font', {weight = 'Light'})
 config.font_size = 20
 config.color_scheme = 'Seti UI (base16)'
@@ -11,6 +14,41 @@ config.underline_position = '200%'
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 999
+
+-- Tab bar width (fill the window)
+--   * Get the cols of the tab, save it to wezterm.GLOBAL.cols
+--   * Calculate the max cols for each tab
+--   * If the max cols is exceeded max_width, use max_width to calculate the max cols for each tab
+function get_max_cols(window)
+  local tab = window:active_tab()
+  local cols = tab:get_size().cols
+  return cols
+end
+wezterm.on(
+  'window-config-reloaded',
+  function(window)
+    wezterm.GLOBAL.cols = get_max_cols(window)
+  end
+)
+wezterm.on(
+  'window-resized',
+  function(window, pane)
+    wezterm.GLOBAL.cols = get_max_cols(window)
+  end
+)
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local title = tab.active_pane.title
+    local full_title = '[' .. tab.tab_index + 1 .. '] ' .. title
+    local pad_length = (wezterm.GLOBAL.cols // #tabs - #full_title) // 2
+    if pad_length * 2 + #full_title > max_width then
+      pad_length = (max_width - #full_title) // 2
+    end
+    return string.rep(' ', pad_length) .. full_title .. string.rep(' ', pad_length)
+  end
+)
 
 -- Window
 config.window_decorations = 'RESIZE'
@@ -19,6 +57,7 @@ config.window_padding = {
   bottom = 0,
 }
 
+-- Fullscreen on startup
 local mux = wezterm.mux
 wezterm.on("gui-startup", function()
   local tab, pane, window = mux.spawn_window{}
