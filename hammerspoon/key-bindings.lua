099b8b7b6c34b185e3133b5de6fdce02749a6d6f
dm4 <sunrisedm4@gmail.com>
2026-08-30 11:23:20 +0800

    feat(hammerspoon): add key bindings for switch monitor input

--- hammerspoon/key-bindings.lua
+++ hammerspoon/key-bindings.lua
@@ -7,3 +7,21 @@ hs.hotkey.bind({"alt", "shift", "cmd"}, "s", toggleSynergy)
 hs.hotkey.bind({"cmd", "alt"}, "r", function()
   hs.reload()
 end)
+
+
+-- Set Monitor Input (DP-1)
+hs.hotkey.bind({"ctrl", "cmd", "alt", "shift"}, "1", function()
+  hs.execute("/opt/homebrew/bin/m1ddc display 1 set input 15")
+end)
+
+
+-- Set Monitor Input (HDMI-1)
+hs.hotkey.bind({"ctrl", "cmd", "alt", "shift"}, "2", function()
+  hs.execute("/opt/homebrew/bin/m1ddc display 1 set input 17")
+end)
+
+
+-- Set Monitor Input (HDMI-2)
+hs.hotkey.bind({"ctrl", "cmd", "alt", "shift"}, "3", function()
+  hs.execute("/opt/homebrew/bin/m1ddc display 1 set input 18")
+end)
